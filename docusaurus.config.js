9f0aa859d7b89ef7a1939d9e328d2dafc7dadfda
MasNopalll <nauval.fauzan69@gmail.com>
2023-05-23 21:49:11 +0700

    logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Utopia Wiki',
+  tagline: 'The Free Reource for All',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -60,7 +60,7 @@ const config = {
         title: 'My Site',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/utopia.jpg',
         },
         items: [
           {
