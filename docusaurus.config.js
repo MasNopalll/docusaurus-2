bb3d9e59dec3fcafeeee462fe5bce31971a8a2e8
MasNopalll <nauval.fauzan69@gmail.com>
2023-05-23 14:16:26 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -109,10 +109,7 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
