7739fad7cb0f7a87e78adc84a774dc6699c0b90f
MasNopalll <nauval.fauzan69@gmail.com>
2023-05-24 11:54:17 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/overview"
           >
-            Docusaurus Tutorial - 5min ⏱️
+            Go to Tutorial
           </Link>
         </div>
       </div>
